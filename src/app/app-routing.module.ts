d5d7824db641a842f50d1e6e1ec1a38654dcde51
madupuis90 <ma.dupuis.90@gmail.com>
2023-05-23 07:00:15 -0400

    progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 const routes: Routes = [
   {
+    path: 'test',
+    component: EmptyRouteComponent,
+  },
+  {
     path: '**',
     component: EmptyRouteComponent,
   }
@@ -13,6 +17,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
 })
 export class AppRoutingModule { }
--- src/main.single-spa.ts
+++ src/main.single-spa.ts
@@ -19,7 +19,7 @@ const lifecycles = singleSpaAngular({
     singleSpaPropsSubject.next(singleSpaProps);
     return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
   },
-  template: '<app-root />',
+  template: '<app-root2 />',
   Router,
   NavigationStart,
   NgZone,
@@ -28,3 +28,37 @@ const lifecycles = singleSpaAngular({
 export const bootstrap = lifecycles.bootstrap;
 export const mount = lifecycles.mount;
 export const unmount = lifecycles.unmount;
+
+// must be called "devtools"
+export const devtools = {
+  overlays: {
+    // selectors is required for overlays to work
+    selectors: [
+      // an array of CSS selector strings, meant to be unique ways to identify the outermost container of your app
+      // you can have more than one, for cases like parcels or different containers for differet views
+      '#my-app',
+      '.some-container .app',
+    ],
+    // options is optional
+    options: {
+      // these options allow you some control over how the overlay div looks/behaves
+      // the listed values below are the defaults
+
+      width: '100%',
+      height: '100%',
+      zIndex: 40,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      color: '#000', // the default for this is actually based on the app's name, so it's dynamic. can be a hex or a CSS color name
+      background: '#000', // the default for this is actually based on the app's name, so it's dynamic. can be a hex or a CSS color name
+      textBlocks: [
+        // allows you to add additional text to the overlay. for example, you can add the name of the team/squad that owns this app
+        // each string in this array will be in a new div
+        // 'blue squad', 'is awesome'
+        // turns into:
+        // <div>blue squad</div><div>is awesome</div>
+      ],
+    },
+  },
+};
